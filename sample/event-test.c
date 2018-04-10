4fd3e4a153cc1c4efea500c2d50eb96d25f25cb0
dingpeilong <77676182@qq.com>
2018-04-10 14:29:48 +0800

    update

--- sample/event-test.c
+++ sample/event-test.c
@@ -11,33 +11,5 @@
 int
 main(int argc, char **argv)
 {
-    struct stat st;
-    char *fifo = "event.fifo";
-    int socket;
-
-    if ( (lstat(fifo, &st)) == 0) {
-        if ((st.st_mode & S_IFMT) == S_IFREG) {
-            errno = EEXIST;
-            perror("lstat");
-            exit(1);
-        }
-    }
-
-    unlink(fifo);
-    if (mkfifo(fifo, 0600) == -1) {
-        perror("mkfifo");
-        exit(1);
-    }
-
-    if ( (socket = open(fifo, O_RDONLY | O_NONBLOCK)) == -1) {
-        perror("open");
-        exit(1);
-    }
-
-    fprintf(stderr, "Write data to %s\n", fifo);
-
-    /* Initialize the event library */
-    event_init();
-
-    return 0;
+    printf("Hello, libevent!\n");
 }
